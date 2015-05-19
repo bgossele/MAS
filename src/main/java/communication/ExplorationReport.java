abd7bf66b57fd3fb7e6f1487ac3c3137ce9e2bc0
Brecht Gosselé <brecht.gossele@gmail.com>
2015-05-19 13:40:33 +0200

    ExplorationReport

--- src/main/java/communication/ExplorationReport.java
+++ src/main/java/communication/ExplorationReport.java
@@ -1,7 +1,23 @@
 package communication;
 
+import java.util.List;
+
+import model.road.Pheromone;
+
 import com.github.rinde.rinsim.core.model.comm.MessageContents;
+import com.github.rinde.rinsim.geom.Point;
 
 public class ExplorationReport implements MessageContents{
+	
+	public final Point pos;
+	
+	public final List<Pheromone> pheromones;
+	
+	public ExplorationReport(Point pos, List<Pheromone> pheromones){
+		this.pos = pos;
+		this.pheromones = pheromones;
+	}
+	
+	
 	
 }
