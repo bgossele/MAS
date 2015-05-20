f6c17844ff8c44d3cae16c9a4fc93ec3e0248f74
Laurens Vanderstraeten <laurensvds@msn.com>
2015-05-20 15:02:32 +0200

    added registering in AntFactory.

--- src/main/java/users/AntFactory.java
+++ src/main/java/users/AntFactory.java
@@ -3,6 +3,7 @@ package users;
 import java.util.ArrayDeque;
 import java.util.Queue;
 
+import com.github.rinde.rinsim.core.SimulatorAPI;
 import com.github.rinde.rinsim.geom.Point;
 
 public class AntFactory {
@@ -10,10 +11,11 @@ public class AntFactory {
 	private static Queue<ExplorationAnt> unusedAntInstances = new ArrayDeque<ExplorationAnt>();
 
 	public static ExplorationAnt build(Point start, Robot mothership,
-			int hopLimit, int id) {
+			int hopLimit, int id, SimulatorAPI sim) {
 		ExplorationAnt ant = unusedAntInstances.poll();
 		if (ant == null) {
 			ant = new ExplorationAnt();
+			sim.register(ant);
 		}
 		ant.reset();
 		ant.set(start, mothership, hopLimit, id);
@@ -21,10 +23,11 @@ public class AntFactory {
 	}
 
 	public static ExplorationAnt build(Point start, Point destination,
-			Robot mothership, int hopLimit, int id) {
+			Robot mothership, int hopLimit, int id, SimulatorAPI sim) {
 		ExplorationAnt ant = unusedAntInstances.poll();
 		if (ant == null) {
 			ant = new ExplorationAnt();
+			sim.register(ant);
 		}
 		ant.reset();
 		ant.set(start, destination, mothership, hopLimit, id);
