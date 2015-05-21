814d66513b5b87a60d75c9257a4d96647278c584
Laurens Vanderstraeten <laurensvds@msn.com>
2015-05-21 17:09:29 +0200

    PointTree

---
+++ src/main/java/model/road/PointTree.java
@@ -0,0 +1,49 @@
+package model.road;
+
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
+
+import com.github.rinde.rinsim.geom.Point;
+
+public class PointTree {
+
+	private PointTree parrent;
+
+	private Point point;
+
+	private Set<PointTree> children = new HashSet<PointTree>();
+
+	private int depth;
+
+	public PointTree(Point point) {
+		this.point = point;
+		this.depth = 0;
+	}
+
+	public PointTree(PointTree parrent, Point point) {
+		this.parrent = parrent;
+		this.point = point;
+		this.depth = this.parrent.getDepth() + 1;
+	}
+
+	public PointTree getParrent() {
+		return parrent;
+	}
+	
+	public Point getPoint() {
+		return point;
+	}
+
+	public Collection<PointTree> getChildren() {
+		return children;
+	}
+
+	public void addChild(PointTree point) {
+		children.add(point);
+	}
+
+	public int getDepth() {
+		return depth;
+	}
+}
---
+++ src/test/java/model/road/PointTreeTest.java
@@ -0,0 +1,49 @@
+package model.road;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import com.github.rinde.rinsim.geom.Point;
+
+public class PointTreeTest {
+
+	private PointTree pointTree1;
+	private PointTree pointTree2;
+	private PointTree pointTree3;
+
+	@Before
+	public void setUp() {
+		pointTree1 = new PointTree(new Point(1, 1));
+		pointTree2 = new PointTree(pointTree1, new Point(2, 2));
+		pointTree3 = new PointTree(pointTree2, new Point(3, 3));
+		pointTree1.addChild(pointTree2);
+		pointTree2.addChild(pointTree3);
+	}
+	
+	@Test
+	public void getParrentTest() {
+		assert (pointTree1.getParrent() == null);
+		assert (pointTree2.getParrent().equals(pointTree1));
+		assert (pointTree3.getParrent().equals(pointTree2));
+	}
+
+	@Test
+	public void getPointTest() {
+		assert (pointTree1.getPoint().equals(new Point(1, 1)));
+		assert (pointTree2.getPoint().equals(new Point(2, 2)));
+	}
+
+	@Test
+	public void childTest() {
+		assert (pointTree1.getChildren().contains(pointTree2));
+		assert (pointTree2.getChildren().contains(pointTree3));
+	}
+
+	@Test
+	public void depthTest() {
+		assert (pointTree1.getDepth() == 0);
+		assert (pointTree2.getDepth() == 1);
+		assert (pointTree3.getDepth() == 3);
+	}
+
+}
